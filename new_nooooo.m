29a335cd7d7e7f057736c041f5ab097a9f336c81
esmyth-V <113724987+esmyth-V@users.noreply.github.com>
2022-11-06 18:55:54 -0500

    Update new_nooooo.m

--- new_nooooo.m
+++ new_nooooo.m
@@ -4,22 +4,25 @@ clear all
 [Bird, Fs_Bird] = audioread('Birds.wav'); 
 birdInfo =audioinfo("Birds.wav");
 numChanel_bird = birdInfo.NumChannels
-audiowrite('Birds.wav', Bird, Fs_Bird)
+%audiowrite('BirdsLow.wav', Bird, Fs_Bird)
+%audiowrite('BirdsHigh.wav', Bird, Fs_Bird)
 
 
 [Drum, Fs_Drum] = audioread('Drum.wav');
 DrumInfo =audioinfo("Drum.wav");
 numChanel_Drum = DrumInfo.NumChannels
-audiowrite('Birds.wav', Drum, Fs_Drum)
+%audiowrite('DrumLow.wav', Drum, Fs_Drum)
+%audiowrite('DrumHigh.wav', Drum, Fs_Drum)
 
 [Speech, Fs_Speech] = audioread('Speech.wav');
 SpeechInfo =audioinfo("Speech.wav");
 numChanel_Speech = SpeechInfo.NumChannels
-audiowrite('Speech.wav', Speech, Fs_Speech)
+%audiowrite('SpeechLow.wav', Speech, Fs_Speech)
+%audiowrite('SpeechHigh.wav', Speech, Fs_Speech)
 
-sound(Bird, Fs_bird)
-sound(Speech,Fs_Speech)
-sound(Drum,Fs_Drum)
+%sound(Bird, Fs_Bird)
+%sound(Speech,Fs_Speech)
+%sound(Drum,Fs_Drum)
 
 %plot
 subplot(3,2,[1,2])
@@ -28,9 +31,11 @@ t = 0:dt_Bird:(length(Bird)*dt_Bird)-dt_Bird;
 plot(t,Bird); xlabel('Seconds'); ylabel('Amplitude');
 
 subplot(3,2,[3,4])
+Drum1 = Drum1(:,1);
 dt_Drum = 1/Fs_Drum;
-t = 0:dt_Drum:(length(Drum)*dt_Drum)-dt_Drum;
-plot(t,Drum); xlabel('Seconds'); ylabel('Amplitude');
+t = 0:dt_Drum:(length(Drum1)*dt_Drum)-dt_Drum;
+plot(t,Drum1); xlabel('Seconds'); ylabel('Amplitude');
+
 
 subplot(3,2,[5,6])
 dt_Speech = 1/Fs_Speech;
