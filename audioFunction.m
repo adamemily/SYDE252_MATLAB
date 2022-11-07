2b7b0cdc5b27e8c560583a48fc0c6943f7366557
esmyth-V <113724987+esmyth-V@users.noreply.github.com>
2022-11-06 22:50:20 -0500

    Update audioFunction.m

--- audioFunction.m
+++ audioFunction.m
@@ -3,7 +3,7 @@ function[Data, Fs, DataInfo, numChannel_Data,  dt_Data, t] = audioFunction(audio
 [Data, Fs] = audioread(audioFile); 
 DataInfo = audioinfo(audioFile);
 numChannel_Data = DataInfo.NumChannels
-%audiowrite('newAudio', Data, Fs);
+%audiowrite('newAudio', Data, Fs); can't seem to get this to run??? like it throws an error
 
 sound(Data, Fs);
 
@@ -12,3 +12,5 @@ t = 0:dt_Data:(length(Data)*dt_Data)-dt_Data;
 plot(t,Data); 
 xlabel('Seconds'); 
 ylabel('Amplitude');
+
+%the og nooooo.m has the code where it calls this function we woulf just need to call the function x amount of times for each plot/audio and all the changing of the FS
