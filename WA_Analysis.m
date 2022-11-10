130485896bdbc73370f75a1db1aec5f101e43ae3
adamemily <Emily.Canada2@gmail.com>
2022-11-10 13:38:51 -0500

    saved audio folder, test file for WA

--- WA_Analysis.m
+++ WA_Analysis.m
@@ -1,13 +1,16 @@
 clc
 clear all 
+hold on
 
-[y_unfiltered, Fs] = audioFunction('Drum.wav');
+[y_unfiltered, Fs] = audioFunction('Speech.wav');
 dt_Data = 1/Fs;
 t = 0:dt_Data:(length(y_unfiltered)*dt_Data)-dt_Data;
-subplot(2, 1, 1)
-plot(t,y_unfiltered); xlabel('Seconds'); ylabel('Amplitude');
+%subplot(2, 1, 1)
+plot(t,y_unfiltered);
 
-window = 100;
+window = 12;
 y_filtered = weightedAvgFunc(y_unfiltered, Fs, window);
-subplot(2, 1, 2)
-plot(t,y_filtered); xlabel('Seconds'); ylabel('Amplitude');
+%subplot(2, 1, 2)
+plot(t,y_filtered); 
+
+xlabel('Seconds'); ylabel('Amplitude');
