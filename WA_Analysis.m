f860c47454c696e449b7d0b1c4e1d71ac1fe40e2
adamemily <Emily.Canada2@gmail.com>
2022-11-10 15:32:11 -0500

    weightedavg tuning audio

--- WA_Analysis.m
+++ WA_Analysis.m
@@ -1,14 +1,14 @@
 clc
 clear all 
-hold on
+%hold on
 
 [y_unfiltered, Fs] = audioFunction('Speech.wav');
 dt_Data = 1/Fs;
 t = 0:dt_Data:(length(y_unfiltered)*dt_Data)-dt_Data;
 %subplot(2, 1, 1)
-plot(t,y_unfiltered);
+%plot(t,y_unfiltered);
 
-window = 12;
+window = 10;
 y_filtered = weightedAvgFunc(y_unfiltered, Fs, window);
 %subplot(2, 1, 2)
 plot(t,y_filtered); 
